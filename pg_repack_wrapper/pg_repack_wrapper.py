7934c1037e03e3eedfff3997e8c1763a86ac7a8c
kmoppel <kaarel.moppel@zalando.de>
2015-11-25 12:51:40 +0100

    --all-dbs and --create-extension flags added + refactor

--- pg_repack_wrapper/pg_repack_wrapper.py
+++ pg_repack_wrapper/pg_repack_wrapper.py
@@ -85,39 +85,76 @@ ORDER BY
     """
 
 
-def shell_exec_with_output(commands):
+def shell_exec_with_output(commands, ok_code=0):
     process = subprocess.Popen(commands, stdout=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT)
     exitcode = process.wait()
     output = process.stdout.read().strip()
-    if exitcode != 0:
-        logging.error('error executing: %s', commands)
+    if exitcode != ok_code:
+        logging.error('Error executing: %s', commands)
         logging.error(output)
     return exitcode, output
 
 
-def get_bloated_tables(min_bloat_ratio=None, min_bloat_size_mb=None):
-    conn = psycopg2.connect(host=args.host, port=args.port, dbname=args.dbname, user=args.username)
+def get_all_dbs():
+    conn = psycopg2.connect(host=args.host, port=args.port, dbname='postgres', user=args.username)
+    conn.autocommit = True
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute("select datname from pg_database where not datistemplate and datallowconn order by 1")
+    return [d['datname'] for d in cur.fetchall()]
+
+
+def get_bloated_tables(dbname, min_bloat_ratio=None, min_bloat_size_mb=None):
+    conn = psycopg2.connect(host=args.host, port=args.port, dbname=dbname, user=args.username)
     conn.autocommit = True
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     cur.execute(SQL_TABLE_BLOAT, {'min_bloat_ratio': min_bloat_ratio, 'min_bloat_size_mb': min_bloat_size_mb})
     return cur.fetchall()
 
 
-def call_pg_repack(table_name_full, args, unknown_args):
-    PG_REPACK_CMD = 'pg_repack'
+def ensure_pgrepack_existance_and_version_equality(dbname, client_version):
+    conn = psycopg2.connect(host=args.host, port=args.port, dbname=dbname, user=args.username)
+    conn.autocommit = True
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
-    PG_REPACK_CMD += ' --table ' + table_name_full
-    for k, v in vars(args).iteritems():
-        if k in ['host', 'port', 'dbname', 'username']:
-            PG_REPACK_CMD += ' --{} {}'.format(k, v)
-    PG_REPACK_CMD += ' ' + ' '.join(unknown_args)
-    logging.info('executing: %s', PG_REPACK_CMD)
+    cur.execute("select extversion from pg_extension where extname = 'pg_repack'")
+    data = cur.fetchone()
+    if not data:
+        if not args.create_extension:
+            logging.info('No pg_repack extension on "%s". Use --create-extension to auto-create', dbname)
+            return False
+        elif args.create_extension:
+            logging.info('Creating pg_repack extension...')
+            if not args.run:
+                return True
+            cur.execute("create extension pg_repack with schema public")
+            if cur.statusmessage != 'CREATE EXTENSION':
+                logging.error('Failed to create pg_repack extension on "%s"', dbname)
+                return False
+            cur.execute("select extversion from pg_extension where extname = 'pg_repack'")
+            data = cur.fetchone()
+
+    extversion = data['extversion']
+    if extversion != client_version:
+        logging.error('Client version (%s) != extension version (%s).', client_version, extversion)
+        return False
+    return True
+
+
+def call_pg_repack(table_name_full, args, unknown_args):
+    pg_repack_cmd = 'pg_repack'
+
+    pg_repack_cmd += ' --table ' + table_name_full
+    pg_repack_cmd += ' ' + (' '.join('--{}={}'.format(k, vars(args)[k]) for k in ('host', 'port', 'dbname', 'username', 'wait_timeout')))
+    pg_repack_cmd = pg_repack_cmd.replace('--wait_timeout', '--wait-timeout')
+    pg_repack_cmd += ' ' + ' '.join(unknown_args)
+    logging.info('Executing: %s', pg_repack_cmd)
     if not args.run:
         return
 
-    retcode, output = shell_exec_with_output(PG_REPACK_CMD)
+    retcode, output = shell_exec_with_output(pg_repack_cmd)
     if retcode != 0 and args.stop_on_error:
         logging.error('exiting because of a processing error')
+        logging.error(output)
         exit(1)
     logging.info(output)
 
@@ -132,48 +169,69 @@ def main():
     argp.add_argument('--help', help='Show help', action='help')
     argp.add_argument('-h', '--host', help='PG host', required=True)
     argp.add_argument('-p', '--port', help='PG port', default=5432, type=int)
-    argp.add_argument('-d', '--dbname', help='PG database', required=True)
     argp.add_argument('-U', '--username', help='PG user', default=os.getenv('USER'))    # password is assumed to be in .pgpass
-    argp.add_argument('-T', '--wait-timeout', help='Max seconds to wait when acquiring an exclusive table lock. pg_repack default=60', required=True)
+    argp.add_argument('-T', '--wait-timeout', help='Max seconds to wait before killing other table users, when acquiring an exclusive table lock. pg_repack default=60', required=True)
+
+    group1 = argp.add_mutually_exclusive_group(required=True)
+    group1.add_argument('-d', '--dbname', help='Database to repack')
+    group1.add_argument('-a', '--all-dbs', action='store_true', help='Repack all non-template DBs')
 
     argp.add_argument('--min-bloat-ratio', help='Min (relative) bloat ratio for table to be considered for re-packing', default=20, type=int)
-    argp.add_argument('--min-bloat-size-mb', help='Min bloat size in MB for table to be considered for re-packing', default=10, type=int)
-    argp.add_argument('-r', '--run', help='Do re-packing. Default is to just display to-be-affected tables', action='store_true')
+    argp.add_argument('--min-bloat-size-mb', help='Min bloat size in MB for table to be considered for re-packing', default=100, type=int)
+    argp.add_argument('-r', '--run', help='Do re-packing. Default is to just display to-be-affected tables. WARNING - repacking could cause significant IO + locking', action='store_true')
     argp.add_argument('-t', '--table', help='Tables to possibly re-pack', action='append')
     argp.add_argument('-q', '--quiet', help='No chat, only errors (Cronjob mode)', action='store_true')
     argp.add_argument('--stop-on-error', help='Exit program on 1st re-packing error', action='store_true')
+    argp.add_argument('--create-extension', help='Create pg_repack extension if missing', action='store_true')
 
     global args
     args, unknown_args = argp.parse_known_args()
 
     logging.basicConfig(level=(logging.ERROR if args.quiet else logging.INFO), format='%(asctime)s (%(levelname)s) %(message)s')
-    logging.info('args: %s, unknown_args: %s', args, unknown_args)
+    logging.info('Args: %s, unknown_args: %s', args, unknown_args)
 
     retcode, out = shell_exec_with_output('which pg_repack')
+    if retcode != 0:
+        logging.error('pg_repack not found on PATH. exiting')
+        exit(1)
     logging.info('Using pg_repack from path: %s', out)
+    retcode, out = shell_exec_with_output('pg_repack --version', ok_code=1)
+    repack_client_version = out.split(' ')[1]
+    logging.info('pg_repack client version: %s', repack_client_version)
 
     if args.run:
-        logging.info('NOT in dry-run mode, sleeping 3s...')
+        logging.info('WARNING - in LIVE mode. Repacking could cause significant IO + locking')
+        logging.info('Sleeping for 3s... (hit CTL+C to exit)')
         time.sleep(3)
 
-    bloated = get_bloated_tables(min_bloat_ratio=args.min_bloat_ratio, min_bloat_size_mb=args.min_bloat_size_mb)
-
-    if not len(bloated):
-        logging.info('No matching tables found')
-        return
-
-    i = 0
-    for b in bloated:
-        if args.table and b['table_name_full'] not in args.table:
+    tables_processed = 0
+    dbs = [args.dbname] if args.dbname else get_all_dbs()
+    for db in dbs:
+        logging.info('Processing database "%s"', db)
+        if not ensure_pgrepack_existance_and_version_equality(db, repack_client_version):
+            logging.info('Skipping "%s" due to missing/different version of pg_repack extension', db)
             continue
-        logging.info('Doing table: "%s" (bloat ratio: %s, bloat_size: %s, table_size %s)', b['table_name_full'],
-                                                                                          b['bloat_ratio'],
-                                                                                          b['bloat_size'],
-                                                                                          b['table_size'])
-        call_pg_repack(b['table_name_full'], args, unknown_args)
-        i += 1
-    logging.info('')
-    logging.info('Finished. %s tables processed.', i)
+
+        bloated = get_bloated_tables(db, min_bloat_ratio=args.min_bloat_ratio, min_bloat_size_mb=args.min_bloat_size_mb)
+        if not len(bloated):
+            logging.info('No matching tables found')
+            continue
+
+        i = 0
+        for b in bloated:
+            if args.table and b['table_name_full'] not in args.table:
+                continue
+            logging.info('Doing table: "%s" (bloat ratio: %s, bloat_size: %s, table_size %s)', b['table_name_full'],
+                                                                                              b['bloat_ratio'],
+                                                                                              b['bloat_size'],
+                                                                                              b['table_size'])
+            call_pg_repack(b['table_name_full'], args, unknown_args)
+            i += 1
+        tables_processed += i
+        logging.info('%s tables processed for "%s"', i, db)
+
+    logging.info('---')
+    logging.info('Finished. %s tables processed.', tables_processed)
 
 
 if __name__ == '__main__':
